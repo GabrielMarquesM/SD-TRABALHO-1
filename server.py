2526fe1c05c8a94049699c46ce6059e96d091d14
GabrielMarquesM <raskeng@gmail.com>
2022-09-14 20:30:21 -0300

    Creating start function in server.py

--- server.py
+++ server.py
@@ -3,29 +3,34 @@ import socket
 
 from expression import Expression
 
-i = 0
+
+def start(host, port):
+    i = 0
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind((HOST, PORT))
+    while True:
+        i += 1
+        print(f"Waiting for expression... \n")
+
+        data, address = s.recvfrom(1024)
+        host, port = address
+
+        exp_deserialized = json.loads(data.decode('utf-8'))
+        exp = Expression(**exp_deserialized)
+
+        print(f"Expression No {i} received!")
+        print(f"Message ID: {exp.get_id()}")
+        print(f"Address: {host} | Port: {port}")
+        try:
+            print("Sending Expression Result...")
+            s.sendto(bytes(exp.to_string(), 'utf-8'), (host, port))
+            print("Result sent succesfully")
+        except:
+            print(f"Timeout for sending Result of Expression No {i}")
+        print()
+
+
 HOST = ''
 PORT = 6789
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind((HOST, PORT))
 
-while True:
-    i += 1
-    print(f"Waiting for expression... \n")
-
-    data, address = s.recvfrom(1024)
-    host, port = address
-
-    exp_deserialized = json.loads(data.decode('utf-8'))
-    exp = Expression(**exp_deserialized)
-
-    print(f"Expression No {i} received!")
-    print(f"Message ID: {exp.get_id()}")
-    print(f"Address: {host} | Port: {port}")
-    try:
-        print("Sending Expression Result...")
-        s.sendto(bytes(exp.to_string(), 'utf-8'), (host, port))
-        print("Result sent succesfully")
-    except:
-        print(f"Timeout for sending Result of Expression No {i}")
-    print()
+start(HOST, PORT)
